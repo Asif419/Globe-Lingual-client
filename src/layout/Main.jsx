459e9e028ae1d94ba042ec9632e9e0a6352fb92c
Asif419 <asshah419@gmail.com>
2023-06-07 03:01:46 +0600

    auth provider

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,11 @@
+import useAuth from "../hooks/useAuth";
 
 const Main = () => {
+  const { user } = useAuth();
   return (
     <div>
       <h2>Main</h2>
-      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-primary">Button {user?.name}</button>
     </div>
   );
 };
