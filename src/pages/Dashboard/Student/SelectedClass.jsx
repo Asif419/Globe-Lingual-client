c37aa3af208fafbaeffb7ac5670ace14ca4b56f9
Asif419 <asshah419@gmail.com>
2023-06-10 20:59:47 +0600

    class delete for user

--- src/pages/Dashboard/Student/SelectedClass.jsx
+++ src/pages/Dashboard/Student/SelectedClass.jsx
@@ -2,24 +2,77 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useAuth from "../../../hooks/useAuth";
 
-const SelectedClass = ({ c, index }) => {
+const SelectedClass = ({ c, index, handleDelete }) => {
+  console.log(c);
+  const { selected_classes_id } = c;
   const { loading } = useAuth();
   const [axiosSecure] = useAxiosSecure();
-  console.log(c);
 
-  const { data: selectedClasses = [], refetch, isLoading: isselectedClassesLoading } = useQuery({
-    queryKey: ['selectedClasses'],
+  const { data: selectedClass = [] } = useQuery({
+    queryKey: ['selectedClass', selected_classes_id],
     enabled: !loading,
     queryFn: async () => {
-      const res = await axiosSecure.get(`/selected-class-from-array/${c}`);
+      const res = await axiosSecure.get(`/selected-class-user/${selected_classes_id}`);
       return res.data;
     }
   })
 
-  console.log(selectedClasses);
+  console.log(selectedClass);
+
+  const { class_name, instructor_name, class_photo_url, class_price, total_seats, enrolled_students } = selectedClass;
 
   return (
-    <tr></tr>
+    <tr>
+      <th>
+        {index}
+      </th>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div>
+            <div className="font-bold">{class_name}</div>
+          </div>
+        </div>
+      </td>
+      <td>
+        <div className="avatar">
+          <div className="mask mask-squircle w-12 h-12">
+            <img src={class_photo_url} alt="Avatar Tailwind CSS Component" />
+          </div>
+        </div>
+      </td>
+      <td>
+        <div className="flex items-center justify-center space-x-3">
+          <div>
+            <div className="font-bold">{class_price}</div>
+          </div>
+        </div>
+      </td>
+      <td>
+        <div className="flex items-center justify-center space-x-3">
+          <div>
+            <div className="font-bold">{instructor_name}</div>
+          </div>
+        </div>
+      </td>
+      <td>
+        <div className="flex items-center justify-center space-x-3">
+          <div>
+            <div className="font-bold">{total_seats}</div>
+          </div>
+        </div>
+      </td>
+      <td>
+        <div className="flex items-center space-x-3 justify-center">
+          <div>
+            <div className="font-bold">{enrolled_students}</div>
+          </div>
+        </div>
+      </td>
+      <td className="flex flex-col gap-2 items-center justify-center text-center">
+        <button onClick={() => handleDelete(c?._id)} className="btn btn-xs bg-red-200">Delete</button>
+        <button className="btn btn-xs bg-green-200">Enroll</button>
+      </td>
+    </tr>
   );
 };
 
--- src/pages/Dashboard/Student/SelectedClasses.jsx
+++ src/pages/Dashboard/Student/SelectedClasses.jsx
@@ -1,17 +1,28 @@
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useUser from "../../../hooks/useUser";
 import useUserClasses from "../../../hooks/useUserClasses";
 import Loading from "../../Shared/Loading/Loading";
 import SelectedClass from "./SelectedClass";
 
 const SelectedClasses = () => {
-  const [userClasses, , isUserClassesLoading] = useUserClasses();
-  const classes = userClasses?.selected_classes_id || [];
-  
-  console.log(userClasses);
+  const [userClasses, refetch, isUserClassesLoading] = useUserClasses();
+  const [userFromDB] = useUser();
+  const user_id = userFromDB?._id;
+  const [axiosSecure] = useAxiosSecure();
 
   if (isUserClassesLoading) {
     return <Loading></Loading>
   }
+
+  const handleDelete = async (class_id,) => {
+    console.log(user_id, class_id);
+    axiosSecure.delete(`/delete-class-from-array/${class_id}`, {
+      class_id
+    })
+    refetch();
+  }
+
+  console.log(userClasses)
 
   return (
     <div className="overflow-x-auto w-full">
@@ -23,6 +34,7 @@ const SelectedClasses = () => {
             <th className="text-start">Class Name</th>
             <th>Class Image</th>
             <th>Price $</th>
+            <th>Instructor Name</th>
             <th>Max seat</th>
             <th>Enrolled Students</th>
             <th>Action</th>
@@ -30,11 +42,11 @@ const SelectedClasses = () => {
         </thead>
         <tbody className="text-center">
           {
-            classes.map((c, index) => <SelectedClass
-              key={c._id}
+            userClasses.map((c, index) => <SelectedClass
+              key={index}
               c={c}
               index={index + 1}
-            // handleChangeRole={handleChangeRole}
+              handleDelete={handleDelete}
             ></SelectedClass>)
           }
         </tbody>
